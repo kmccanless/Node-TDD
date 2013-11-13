9d385cdab2ff0dd8cee768ab1250f198b1f4e25d
Keith <common@mccanless.us>
2013-11-12 20:21:58 -0500

    with addThreeDigits passing

--- calc.js
+++ calc.js
@@ -2,6 +2,9 @@ function Calculator() {
     this.addTwoDigits = function (digit1,digit2) {
         return(digit1 + digit2);
     }
+    this.addThreeDigits = function (digit1,digit2,digit3) {
+        return(digit1 + digit2 + digit3);
+    }
 }
 
 module.exports = new Calculator();
