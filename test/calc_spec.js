76d37ad9de9619b56583522cf82b17595a626b6e
Keith <common@mccanless.us>
2013-11-12 19:59:42 -0500

    initial commit of project with modules and calc_spec

---
+++ test/calc_spec.js
@@ -0,0 +1,10 @@
+var     chai = require('chai')
+    ,   calc = require('../calc.js');
+
+var expect = chai.expect;
+
+describe('addition',function() {
+    it('should correctly sum 2 digits',function(){
+        expect(calc(2,2)).to.equal(4);
+    })
+})
