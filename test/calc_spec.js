3818f2bcc6840c2079fe523fae87f7115f26cfa0
Keith <common@mccanless.us>
2013-11-15 14:47:54 -0500

    passing tests for two digit addition

--- test/calc_spec.js
+++ test/calc_spec.js
@@ -7,8 +7,4 @@ describe('addition',function() {
     it('should correctly sum 2 digits',function(){
         expect(calc.addTwoDigits(2,2)).to.equal(4);
     })
-
-    it('should correctly sum 3 digits',function(){
-        expect(calc.addThreeDigits(2,2,2)).to.equal(6);
-    })
 })
