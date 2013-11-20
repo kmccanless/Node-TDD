695b0cac20d6467d160d3a51ef2d1d55ac493789
Keith <common@mccanless.us>
2013-11-20 13:05:41 -0500

    cleaned up the code

--- MockLib.js
+++ MockLib.js
@@ -1,7 +1,5 @@
 function Mock() {
-    this.sync = function () {
-        return true;
-    };
+
     this.asyncWithCallbacks = function(cb) {
         setTimeout(function(){
             cb(null,true);
--- test/MockLib_spec.js
+++ test/MockLib_spec.js
@@ -1,16 +1,16 @@
 var     chai = require("chai")
-    ,   chaiAsPromised = require("chai-as-promised")
     ,   mockLib = require('../MockLib.js')
 
-var expect = chai.expect,should = chai.should();
+var expect = chai.expect;
 
 
 describe('test asyncronous functions', function() {
+
     it('should return true via a callback', function(done) {
             mockLib.asyncWithCallbacks(function(err,result){
               expect(err).to.equal(null);
               expect(result).to.be.true;
-                done();
+              done();
             })
         }
     );
